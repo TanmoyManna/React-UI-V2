19dcdd8eb5396a4f2c3dfb51f6ffe687fc493186
Tanmoy Manna <tanmoymanna@Tanmoys-MacBook-Air.local>
2025-02-06 00:40:50 +0530

    headers

--- json/assets/index.ts
+++ json/assets/index.ts
@@ -4,7 +4,14 @@
 export default {
     Landingbannerimage:"/assets/images/landingbannerImage.png",
     logoimage:"/assets/images/fullLogo.png",
-    burgerIcon:"/assets/images/burger.jpeg"
+    burgerIcon:"/assets/images/burger.jpeg",
+    shoeIcon:"/assets/images/shoe2.png",
+    specialIcon:"/assets/images/sp.jpeg",
+    limitedIcon:"/assets/images/limited.jpeg",
+    exclusiveIcon:"/assets/images/exclusive.jpeg",
+    sneakersIcon:"/assets/images/sneakers.jpeg"
+
+
 
 
 };
